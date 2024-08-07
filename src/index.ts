24203ace383ca7320e9c3c655b9da9d73a07a298
B <bkashishh07@gmail.com>
2024-08-07 14:58:33 +0530

    final commit

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,8 @@ import mainrouter from "./routes/index";
 
 app.use("/order", mainrouter);
 
-// app.get("*", (req, res) => {
-//   res.status(404).send("Not Found");
-// });
-
-app.get("/", function (req: Request, res: Response) {
-  res.send({
-    Name: "B ASHISH KARAGOND",
-    SKILLS:
-      "Next.js,React.js,Node.js,Express, Typescript,Javascript,Redis,Docker,Postgresql,Prisma,Kubernatives-currently learning ",
-  });
+app.get("*", (req, res) => {
+  res.status(404).send("Not Found");
 });
 
 app.listen(PORT, function () {
