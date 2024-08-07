c395904c59b2caf02b1984f9ea69ad23ce176a8c
B <bkashishh07@gmail.com>
2024-08-07 12:17:42 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,10 @@ import mainrouter from "./routes/index";
 
 app.use("/order", mainrouter);
 
+app.get("*", (req, res) => {
+  res.status(404).send("Not Found");
+});
+
 app.listen(PORT, function () {
   console.log(`server listening on port ${PORT}`);
 });
-
-// app.use("/order", mainrouter);
