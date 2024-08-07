4f1fad7bf39beff5ec69063ca85b571a778093b7
B <bkashishh07@gmail.com>
2024-08-07 14:56:43 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 app.use(express.json());
@@ -9,8 +10,16 @@ import mainrouter from "./routes/index";
 
 app.use("/order", mainrouter);
 
-app.get("*", (req, res) => {
-  res.status(404).send("Not Found");
+// app.get("*", (req, res) => {
+//   res.status(404).send("Not Found");
+// });
+
+app.get("/", function (req: Request, res: Response) {
+  res.send({
+    Name: "B ASHISH KARAGOND",
+    SKILLS:
+      "Next.js,React.js,Node.js,Express, Typescript,Javascript,Redis,Docker,Postgresql,Prisma,Kubernatives-currently learning ",
+  });
 });
 
 app.listen(PORT, function () {
